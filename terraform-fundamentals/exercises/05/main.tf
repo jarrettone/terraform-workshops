93865ebcbdf9da20d88fbeff590752d25c237b1f
Jarrett Lee <jarrettone@users.noreply.github.com>
2021-09-24 20:20:08 +0000

    exercise 5 complete

--- terraform-fundamentals/exercises/05/main.tf
+++ terraform-fundamentals/exercises/05/main.tf
@@ -5,8 +5,15 @@
 # It can also pull credentials and the region to use from environment variables, which we have set, so we'll use those
 provider "aws" {}
 
+provider "aws" {
+  region  = var.region_alt
+  alias   = "alternate"
+}
+
 # declare a resource block so we can create something.
 resource "aws_s3_bucket" "student_bucket_alt" {
   bucket = "tf-fundy-${var.student_alias}-alt"
+  provider = aws.alternate
+
 }
 
